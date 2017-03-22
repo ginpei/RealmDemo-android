2dbdc0c6e164f3e10dd94628846f3c14cd3d5cc5
ginpei <ginpei@gmail.com>
2017-03-22 00:14:56 -0700

    create an application class to prepare Realm only once

---
+++ app/src/main/java/info/ginpei/realmdemo/MyApplication.java
@@ -0,0 +1,14 @@
+package info.ginpei.realmdemo;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
