e622a34c82f18c9ed50e9ca5c4e99ea847116c48
ginpei <ginpei@gmail.com>
2017-03-21 23:48:23 -0700

    lambda expressions

--- app/src/main/java/info/ginpei/realmdemo/MainActivity.java
+++ app/src/main/java/info/ginpei/realmdemo/MainActivity.java
@@ -2,6 +2,7 @@ package info.ginpei.realmdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ignored) {
+            }
+
+            runOnUiThread(() -> {
+                Toast.makeText(MainActivity.this, "Hello!", Toast.LENGTH_SHORT).show();
+            });
+        }).start();
     }
 }
